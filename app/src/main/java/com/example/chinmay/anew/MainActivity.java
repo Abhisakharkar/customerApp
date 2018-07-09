bc7918ad50604c3778856737f742221c98125bf7
chinmaybad <chinmaybadhe15@gmail.com>
2018-07-09 18:48:21 +0530

    ToolTip

--- app/src/main/java/com/example/chinmay/anew/MainActivity.java
+++ app/src/main/java/com/example/chinmay/anew/MainActivity.java
@@ -9,6 +9,7 @@ import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
@@ -21,6 +22,7 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.tooltip.Tooltip;
 
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -61,6 +63,7 @@ public class MainActivity extends AppCompatActivity {
             actionBar.setTitle(place);
         }
 
+
         mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
         mRecyclerView.setHasFixedSize(true);
         mRecyclerView.setItemViewCacheSize(20);
@@ -72,6 +75,7 @@ public class MainActivity extends AppCompatActivity {
         mRecyclerView.addItemDecoration(new VerticalSpaceItemDecoration(0));
         mRecyclerView.addItemDecoration(new DividerItemDecoration(MainActivity.this, R.drawable.divider));
         madapter = new MyAdapter(this);
+        showTooltip(R.id.my_toolbar, Gravity.BOTTOM);
         mRecyclerView.setAdapter(madapter);
         mRecyclerView.addOnItemTouchListener(
                 new RecyclerItemClickListener(this, mRecyclerView ,new RecyclerItemClickListener.OnItemClickListener() {
@@ -82,59 +86,75 @@ public class MainActivity extends AppCompatActivity {
 
                     @Override
                     public void onLongItemClick(View view, int position) {
-
-
                     }
-
 
                 })
         );
 
+    }
 
-
-        final android.support.v7.app.AlertDialog.Builder builder = new android.support.v7.app.AlertDialog.Builder(this);
-
-        // Get the layout inflater
-        LayoutInflater inflater = this.getLayoutInflater();
-        final View prompt = inflater.inflate(R.layout.dialog_pay, null);
-
-
-        // Inflate and set the layout for the dialog
-        // Pass null as the parent view because its going in the dialog layout
-        builder.setView(prompt);
-        Button yes=(Button)prompt.findViewById(R.id.yes);
-        Button change=(Button)prompt.findViewById(R.id.change);
-
-        yes.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "YES", Toast.LENGTH_SHORT).show();
-                d.cancel();
-
-            }
-        });
-        change.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "Change", Toast.LENGTH_SHORT).show();
-                d.cancel();
-            }
-        });
-
-         d=builder.create();
-
-        d.show();
-
-//        Button pbutton = d.getButton(DialogInterface.BUTTON_POSITIVE);
-//        pbutton.setBackgroundColor(Color.parseColor("#000000"));
-
-
-
-
-
-
-// Add the request to the RequestQueue.
-
-
+    private void showTooltip(int my_toolbar, int top)
+    {
+        Tooltip tooltip=new Tooltip.Builder(toolbar)
+                .setText("If you aren't located right, please select your location here")
+                .setTextColor(Color.WHITE)
+                .setGravity(top)
+                .setDismissOnClick(true)
+                .setCancelable(true)
+                .setBackgroundColor(Color.parseColor("#648ab4"))
+                .setTextSize(20f).show();
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//        final android.support.v7.app.AlertDialog.Builder builder = new android.support.v7.app.AlertDialog.Builder(this);
+//
+//        // Get the layout inflater
+//        LayoutInflater inflater = this.getLayoutInflater();
+//        final View prompt = inflater.inflate(R.layout.dialog_pay, null);
+//
+//
+//        // Inflate and set the layout for the dialog
+//        // Pass null as the parent view because its going in the dialog layout
+//        builder.setView(prompt);
+//        Button yes=(Button)prompt.findViewById(R.id.yes);
+//        Button change=(Button)prompt.findViewById(R.id.change);
+//
+//        yes.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Toast.makeText(MainActivity.this, "YES", Toast.LENGTH_SHORT).show();
+//                d.cancel();
+//
+//            }
+//        });
+//        change.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Toast.makeText(MainActivity.this, "Change", Toast.LENGTH_SHORT).show();
+//                d.cancel();
+//            }
+//        });
+//
+//         d=builder.create();
+//
+//        d.show();
